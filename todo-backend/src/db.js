948a3b79ef2888f1bb6eb19790ea83997f7ba40e
EC2 Default User <ec2-user@ip-10-0-11-140.eu-north-1.compute.internal>
2026-04-01 16:32:07 +0000

    DB connected to RDS

--- todo-backend/src/db.js
+++ todo-backend/src/db.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'todo-db.cjsqwsq405r5.eu-north-1.rds.amazonaws.com',
-  port:     parseInt(process.env.DB_PORT || '5432'),
-  user:     process.env.DB_USER     || 'postgres',
+  host: process.env.DB_HOST || 'todo-db.cjsqwsq405r5.eu-north-1.rds.amazonaws.com',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'devops123',
-  database: process.env.DB_NAME     || 'todo-db',
+  database: process.env.DB_NAME || 'tododb',
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
