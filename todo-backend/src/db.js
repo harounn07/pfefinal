9a8a3c8f53b9cf20319d9ea7324b25f5860be1ba
pedroc71 <garrebzied1@gmail.com>
2026-04-01 14:51:38 +0200

    test RDS

--- todo-backend/src/db.js
+++ todo-backend/src/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
+  host:     process.env.DB_HOST     || 'todo-db.cjsqwsq405r5.eu-north-1.rds.amazonaws.com',
   port:     parseInt(process.env.DB_PORT || '5432'),
   user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME     || 'tododb',
+  password: process.env.DB_PASSWORD || 'devops123',
+  database: process.env.DB_NAME     || 'todo-db',
 });
 
 pool.on('connect', () => {
