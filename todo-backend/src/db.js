3b34d5556dc306d04fd1ea9117a8f4fe441aa0ca
pedroc71 <garrebzied1@gmail.com>
2026-04-11 19:40:56 +0200

    Test fix SQube Relaibility

--- todo-backend/src/db.js
+++ todo-backend/src/db.js
@@ -2,24 +2,25 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'todo-db.cjsqwsq405r5.eu-north-1.rds.amazonaws.com',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD  ,
-  database: process.env.DB_NAME || 'tododb',
+  host: process.env.DB_HOST,
+  port: Number.parseInt(process.env.DB_PORT ?? '5432', 10),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.DB_SSL === 'true'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
+// Connection success
 pool.on('connect', () => {
   console.log('[DB] Connected to PostgreSQL');
 });
 
+// Better error handling
 pool.on('error', (err) => {
   console.error('[DB] Unexpected error:', err.message);
-  process.exit(-1);
 });
 
 module.exports = pool;
