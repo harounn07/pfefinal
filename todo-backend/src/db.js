1d3a7357a7e444fe6315cd0a181448640fe2d2e4
pedroc71 <garrebzied1@gmail.com>
2026-04-11 19:25:46 +0200

    Test

--- todo-backend/src/db.js
+++ todo-backend/src/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST || 'todo-db.cjsqwsq405r5.eu-north-1.rds.amazonaws.com',
   port: parseInt(process.env.DB_PORT || '5432'),
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD  ,
+  password: process.env.DB_PASSWORD || "devops123" ,
   database: process.env.DB_NAME || 'tododb',
 
   ssl: {
